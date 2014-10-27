774b0640b07c9a087cc15469049b4d5d8d8178b5
Glacier759 <OurHom.759@gmail.com>
2014-10-27 12:33:20 +0800

    增加getProxyList方法返回List<HttpHost>

--- GetProxy/src/main/java/com/glacier/proxy/GetProxy.java
+++ GetProxy/src/main/java/com/glacier/proxy/GetProxy.java
@@ -1,19 +1,15 @@
 package com.glacier.proxy;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.googlecode.aviator.AviatorEvaluator;
+import org.apache.http.HttpHost;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by glacier on 14-10-27.
@@ -24,70 +20,83 @@ public class GetProxy {
         seedURL = "http://www.pachong.org";
 
         GetProxy obj = new GetProxy();
-        //obj.getProxyList();
-        obj.test();
-    }
-
-    public void test() {
-        try {
-            Document document = Jsoup.connect(seedURL).get();
-            Elements jsEles = document.select("head").select("script");
-            String data = jsEles.last().data();
-
-            String[] scriptVar = data.replaceAll("var ", "").split(";");
-            HashMap<String,Integer> ansMap = new HashMap<String, Integer>();
-            for ( String var:scriptVar ) {
-                String keyStr = var.substring(0,var.indexOf('='));
-                String valueStr = var.substring(var.indexOf('=')+1);
-
-                String resultKey = judge(valueStr, ansMap);
-                if ( resultKey != null )
-                    valueStr = valueStr.replace(resultKey, ansMap.get(resultKey).toString());
-
-                Long value = (Long)AviatorEvaluator.execute(valueStr);
-                ansMap.put(keyStr, new Integer(value.intValue()));
-            }
-
-            Elements proxyEles = document.select("tr[data-id]");
-            for ( Element proxyEle:proxyEles ) {
-                String ip = proxyEle.select("td").get(1).text();
-                String portStr = proxyEle.select("script").first().data();
-                portStr = portStr.substring(portStr.indexOf('('), portStr.lastIndexOf(')')+1);
-                String resultKey = judge(portStr, ansMap);
-                if ( resultKey != null )
-                    portStr = portStr.replace(resultKey, ansMap.get(resultKey).toString());
-                Long portLong = (Long)AviatorEvaluator.execute(portStr);
-                String port = portLong.toString();
-                System.out.println(ip+":"+port);
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
+        HashMap<String,String> proxyMap = obj.getProxyMap();
+        for ( String proxyIP:proxyMap.keySet() ) {
+            System.out.println(proxyIP+":"+proxyMap.get(proxyIP));
         }
     }
 
-    public String judge( String str, HashMap<String,Integer> ansMap ) {
+    public List<HttpHost> getProxyList() {
+        try {
+            HashMap<String, String> proxyMap = getProxyMap();
+            List<HttpHost> proxyList = new ArrayList<HttpHost>();
+            for ( String proxyIP:proxyMap.keySet() ) {
+                String proxyPort = proxyMap.get(proxyIP);
+                HttpHost httpHost = new HttpHost(proxyIP, new Integer(proxyPort));
+                proxyList.add(httpHost);
+            }
+            return proxyList;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public HashMap<String,String> getProxyMap() {
+        try {
+            Document document = Jsoup.connect(seedURL)
+                    .userAgent("Mozilla")
+                    .cookie("auth", "token")
+                    .timeout(30000)
+                    .get();
+            Elements jsEles = document.select("head").select("script");
+            String data = jsEles.last().data();
+
+            HashMap<String,Integer> ansMap = getJSVar(data);
+            HashMap<String,String> proxyMap = new HashMap<String, String>();
+            Elements proxyEles = document.select("tr[data-id]");    //遍历每一个proxy代码块
+            for ( Element proxyEle:proxyEles ) {
+                String ip = proxyEle.select("td").get(1).text();    //获得proxy ip
+                String portStr = proxyEle.select("script").first().data();  //获得端口生成所需的js代码
+                portStr = portStr.substring(portStr.indexOf('('), portStr.lastIndexOf(')')+1);
+                String resultKey = getSubStr(portStr, ansMap);
+                if ( resultKey != null )
+                    portStr = portStr.replace(resultKey, ansMap.get(resultKey).toString());     //将变量替换为数字形成表达式
+                Long portLong = (Long)AviatorEvaluator.execute(portStr);    //表达式计算获得端口号
+                String port = portLong.toString();
+                proxyMap.put(ip, port);
+            }
+            return proxyMap;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //解析获得页面中JS各变量的值
+    private HashMap<String,Integer> getJSVar( String data ) {
+        String[] scriptVar = data.replaceAll("var ", "").split(";");
+        HashMap<String,Integer> ansMap = new HashMap<String, Integer>();
+        for ( String var:scriptVar ) {
+            String keyStr = var.substring(0,var.indexOf('='));
+            String valueStr = var.substring(var.indexOf('=')+1);
+
+            String resultKey = getSubStr(valueStr, ansMap);
+            if ( resultKey != null )
+                valueStr = valueStr.replace(resultKey, ansMap.get(resultKey).toString());
+
+            Long value = (Long)AviatorEvaluator.execute(valueStr);
+            ansMap.put(keyStr, new Integer(value.intValue()));
+        }
+        return ansMap;
+    }
+
+    //判断字符串str中是否存在集合ansMap中的键值  存在返回该键值  不存在返回null
+    private String getSubStr( String str, HashMap<String,Integer> ansMap ) {
         for ( String key:ansMap.keySet() ) {
             if ( str.contains(key) )
                 return key;
         }
         return null;
-    }
-
-    public void getProxyList() {
-        try {
-            URL link = new URL(seedURL);
-            WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
-
-            webClient.getCookieManager().setCookiesEnabled(true);
-            webClient.setJavaScriptEnabled(true);
-            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-            webClient.setTimeout(10000);
-            webClient.setThrowExceptionOnScriptError(false);
-
-            HtmlPage htmlPage = webClient.getPage(link);
-            System.out.println(htmlPage.getWebResponse().getContentAsString());
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
