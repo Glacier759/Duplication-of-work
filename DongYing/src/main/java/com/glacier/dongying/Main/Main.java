dbb3ba252184b22772f4e64392080a169dad4326
Glacier759 <OurHom.759@gmail.com>
2014-11-08 13:08:52 +0800

    增加对东营吧的抓取

--- DongYing/src/main/java/com/glacier/dongying/Main/Main.java
+++ DongYing/src/main/java/com/glacier/dongying/Main/Main.java
@@ -13,16 +13,16 @@ import us.codecraft.webmagic.scheduler.component.BloomFilterDuplicateRemover;
  */
 public class Main {
     public static void main(String[] args) {
-        //Spider jobSpider = Spider.create(new BBSPageProcessor());     //BBS
+        Spider jobSpider = Spider.create(new BBSPageProcessor());     //BBS
         //Spider jobSpider = Spider.create(new TiebaPageProcessor());     //Tieba
         //Spider jobSpider = Spider.create(new ShengliPageProcessor());     //ShengliShequ
-        Spider jobSpider = Spider.create(new DongyingPageProcessor());
+        //Spider jobSpider = Spider.create(new DongyingPageProcessor());    //东营吧
         jobSpider.setScheduler(new QueueScheduler()
         .setDuplicateRemover(new BloomFilterDuplicateRemover(100000000)));
-        //jobSpider.addUrl("http://club.dzwww.com/forum-223-1.html");     //BBS
+        jobSpider.addUrl("http://club.dzwww.com/forum-223-1.html");     //BBS
         //jobSpider.addUrl("http://tieba.baidu.com/f?kw=%B6%AB%D3%AA");     //Tieba
         //jobSpider.addUrl("http://www.slit.cn/bbs/forum.php");           //ShengliShequ
-        jobSpider.addUrl("http://www.dy8.com/");
+        //jobSpider.addUrl("http://www.dy8.com/");                      //东营吧
         jobSpider.thread(1);
         jobSpider.run();
     }
